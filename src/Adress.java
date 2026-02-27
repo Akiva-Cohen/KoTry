3ba551f9f0ad6a63efc8a03684fee7febe07bef4
Akiva Cohen <Akivaecohen@gmail.com>
2026-02-27 09:48:05 -0500

    add adress printing

--- src/Adress.java
+++ src/Adress.java
@@ -4,4 +4,12 @@ public class Adress {
     public String city;
     public String state;
     public String zip;
+    public String toString() {
+        String out = "";
+        out = out + name + "\n";
+        out = out + address + "\n";
+        out = out + city + ", " + state + "\n";
+        out = out + zip + "\n";
+        return out;
+    }
 }
