fc6245dbb4b8808edba10a7bd0351cfc9b3edd89
Akiva Cohen <Akivaecohen@gmail.com>
2026-02-27 10:47:45 -0500

    generalized to allow inputs in text (and to expand to all forms of input), so it does not have to pull from a file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Grade orders = Parser.parse("jsons/testStudents.json", Grade.class);
+        Grade orders = Parser.parsePath("jsons/testStudents.json", Grade.class);
         System.out.println(orders);
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -7,14 +7,20 @@ public class Parser {
     static {
         thing = new Gson();
     }
-    public static <type> type parse(String path, Class<type> type) {
+
+    public static <type> type parsePath(String path, Class<type> type) {
         String json = new String();
+
         try {
             json = new String(Files.readAllBytes(Paths.get(path)));
         } catch (Exception e) {
             System.out.println("you had an exception");
         }
-        type output = thing.fromJson(json, type);
-        return output;
-        }
+        
+        return parse(json,type);
+    }
+
+    public static <type> type parse(String text, Class<type> type) {
+        return thing.fromJson(text,type);
+    }
 }
